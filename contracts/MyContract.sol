69185776a77537a5959ac65c6edbbf55ada8150d
drasticstatic <drasticstatic@gmail.com>
2025-05-02 02:20:31 -0400

    Test from iMac_add remaining files back

---
+++ contracts/MyContract.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.8.4;
+
+contract MyContract {
+    string value; // "value=name"
+
+    constructor() {
+        value = "Hello, world!";
+    }
+
+    function get() public view returns(string memory) {
+        return value; 
+    }
+
+    function set(string memory _value) public {
+        value = _value; //underscore refers to local value - _convention 2 differenciate bewtween local & state varibale
+    }
+}
